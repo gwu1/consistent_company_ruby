aa70f33b9ba3a86a1cb125c410824a3aba8a6fd5
Gene Wu <gwu@demystdata.com>
2016-08-18 19:25:45 +0800

    Handling parenthesis & more on str_replace

--- lib/consistent_company_ruby/instance_methods.rb
+++ lib/consistent_company_ruby/instance_methods.rb
@@ -1,9 +1,9 @@
-module InstanceMethods
+module ConsistentCompanyRuby
   module InstanceMethods
-    def namer company
-      return company if company == ''
+    def namer# company
+      return self if self == ''
 
-      in_string = company.dup
+      in_string = self.dup
 
       # while processing we turn & = AND, + = PLUS
       # and we add space at front and back
@@ -12,9 +12,64 @@ module InstanceMethods
 
       in_string = in_string.upcase
       in_string.strip!
-      in_string.gsub(/\(|\)/,' ')
 
-      return_string = nil
+      # in_string.gsub(/\(|\)/,' ')
+      num_lefts = 0
+      num_rights = 0
+      left1 = -1
+      right1 = -1
+      left2 = -1
+      right2 = -1
+      len = in_string.size
+
+      (0..len-1).each do |i|
+        if in_string[i] == '('
+          num_lefts += 1
+          if num_lefts == 1
+            left1 = i
+          elsif num_lefts == 2
+            left2 = i
+          end
+        elsif in_string[i] == ')'
+          num_rights += 1
+          if num_rights == 1
+            right1 = i
+          elsif num_rights == 2
+            right2 = i
+          end
+        end
+      end
+
+      if num_lefts == 0 || in_string[0] == '('
+        # Do Nothing
+	    elsif num_lefts == 1
+  	  	if right1 > left1
+          # ..(xx).. -> ....
+          in_string = "#{in_string[0..left1-1]}#{in_string[right1+1..len-1]}"
+        else
+          # ..(xx -> ..
+          in_string = in_string[0..left1-1]
+        end
+      elsif num_lefts == 2
+        if (left1 < right1) && (right1 < left2) && (left2 < right2)
+          # ..(xx)..(xx).. -> ......
+          in_string = "#{in_string[0..left1-1]}#{in_string[right1+1..left2-1]}#{in_string[right2+1..len-1]}"
+        elsif (left1 < left2) && (left2 < right1) && (right1 < right2)
+          # ..(xx(xx)xx).. -> ....
+          in_string = "#{in_string[0..left1-1]}#{in_string[right2+1..len-1]}"
+        elsif (left1 < right1) && (right1 < left2) && (right2 == -1)
+          # ..(xx)..(xx -> ....
+          in_string = "#{in_string[0..left1-1]}#{in_string[right1+1..left2-1]}"
+        elsif (left1 < left2) && (left2 < right1) && (right2 == -1)
+          # ..(xx(xx)xx -> ..
+          in_string = in_string[0..left1-1]
+        elsif (right1 == -1) && (right2 == -1)
+          # ..(xx(xx -> ..
+          in_string = in_string[0..left1-1]
+        end
+      end
+
+      return_string = ''
       (0..in_string.size-1).each do |i|
         ch = in_string[i]
         asc = ch.ord
@@ -45,11 +100,12 @@ module InstanceMethods
                return_string[return_string.size-1] != ' '
           return_string = "#{return_string} "
         end
-        #puts "#{i}#{ch}#{asc}:#{return_string}"
+        # puts "#{i}#{ch}#{asc}:#{return_string}"
       end
 
-      return_string.gsub!(' AND ', ' & ')
-      return_string.gsub!('AND', ' & ')
+      return '' if return_string.nil? || return_string == ''
+
+      str_replace return_string, ' AND ', ' & '
 
       return_string.strip!
       return_string = transform_company return_string
@@ -59,60 +115,61 @@ module InstanceMethods
     end
 
     def transform_company res
-      res.gsub! " THE ", " "
-      res.gsub! " ONE ", " 1 "
-      res.gsub! " TWO ", " 2 "
-      res.gsub! " TO ", " 2 "
-      res.gsub! " THREE ", " 3 "
-      res.gsub! " FOUR ", " 4 "
-      res.gsub! " FOR ", " 4 "
-      res.gsub! " FIVE ", " 5 "
-      res.gsub! " SIX ", " 6 "
-      res.gsub! " SEVEN ", " 7 "
-      res.gsub! " EIGHT ", " 8 "
-      res.gsub! " NINE ", " 9 "
-      res.gsub! " TEN ", " 10 "
-      res.gsub! " ELEVEN ", " 11 "
+      res = " #{res} "
+      str_replace res,  " THE ", " "
+      str_replace res,  " ONE ", " 1 "
+      str_replace res,  " TWO ", " 2 "
+      str_replace res,  " TO ", " 2 "
+      str_replace res,  " THREE ", " 3 "
+      str_replace res,  " FOUR ", " 4 "
+      str_replace res,  " FOR ", " 4 "
+      str_replace res,  " FIVE ", " 5 "
+      str_replace res,  " SIX ", " 6 "
+      str_replace res,  " SEVEN ", " 7 "
+      str_replace res,  " EIGHT ", " 8 "
+      str_replace res,  " NINE ", " 9 "
+      str_replace res,  " TEN ", " 10 "
+      str_replace res,  " ELEVEN ", " 11 "
 
-      res.gsub! " FIRST ", " 1ST "
-      res.gsub! " SECOND ", " 2ND "
-      res.gsub! " THIRD ", " 3RD "
-      res.gsub! " FOURTH ", " 4TH "
-      res.gsub! " FIFTH ", " 5TH "
-      res.gsub! " SIXTH ", " 6TH "
-      res.gsub! " SEVENTH ", " 7TH "
-      res.gsub! " EIGHTH ", " 8TH "
-      res.gsub! " NINTH ", " 9TH "
-      res.gsub! " TENTH ", " 10TH "
-      res.gsub! " CENTRE ", " CTR "
-      res.gsub! " CENTER ", " CTR "
-      res.gsub! " CNTR ", " CTR "
-      res.gsub! " CENT ", " CTR "
-      res.gsub! " CENTR ", " CTR "
-      res.gsub! " AUTOMOTIVE ", " AUTO "
-      res.gsub! " AUTOMOBILE ", " AUTO "
-      res.gsub! " AUTOS ", " AUTO "
-      res.gsub! " AVENUE ", " AVE "
-      res.gsub! " DRIVE ", " DR "
-      res.gsub! " PHOTOGRAPHY ", " PHOTO "
-      res.gsub! " BROTHERS ", " BROS "
-      res.gsub! " TECHNOLOGY ", " TEC "
-      res.gsub! " TECH ", " TEC "
-      res.gsub! " TELEVISION ", " TV "
-      res.gsub! " INFORMATION ", " INFO "
-      res.gsub! " SOCIETY ", " SOC "
-      res.gsub! " DEPARTMENT ", " DEPT "
-      res.gsub! " REGIONAL ", " REG "
-      res.gsub! " REGION ", " REG "
-      res.gsub! " AUTHORITY ", " AUTH "
-      res.gsub! " NATIONAL ", " NATL "
-      res.gsub! " INTERNATIONAL ", " INT "
-      res.gsub! " INTERNATION ", " INT "
-      res.gsub! " INTL ", " INT "
-      res.gsub! " MARKETING ", " MKT "
-      res.gsub! " MKTG ", " MKT "
-      res.gsub! " MANAGEMENT ", " MGT "
-      res.gsub! " MGMT ", " MGT "
+      str_replace res,  " FIRST ", " 1ST "
+      str_replace res,  " SECOND ", " 2ND "
+      str_replace res,  " THIRD ", " 3RD "
+      str_replace res,  " FOURTH ", " 4TH "
+      str_replace res,  " FIFTH ", " 5TH "
+      str_replace res,  " SIXTH ", " 6TH "
+      str_replace res,  " SEVENTH ", " 7TH "
+      str_replace res,  " EIGHTH ", " 8TH "
+      str_replace res,  " NINTH ", " 9TH "
+      str_replace res,  " TENTH ", " 10TH "
+      str_replace res,  " CENTRE ", " CTR "
+      str_replace res,  " CENTER ", " CTR "
+      str_replace res,  " CNTR ", " CTR "
+      str_replace res,  " CENT ", " CTR "
+      str_replace res,  " CENTR ", " CTR "
+      str_replace res,  " AUTOMOTIVE ", " AUTO "
+      str_replace res,  " AUTOMOBILE ", " AUTO "
+      str_replace res,  " AUTOS ", " AUTO "
+      str_replace res,  " AVENUE ", " AVE "
+      str_replace res,  " DRIVE ", " DR "
+      str_replace res,  " PHOTOGRAPHY ", " PHOTO "
+      str_replace res,  " BROTHERS ", " BROS "
+      str_replace res,  " TECHNOLOGY ", " TEC "
+      str_replace res,  " TECH ", " TEC "
+      str_replace res,  " TELEVISION ", " TV "
+      str_replace res,  " INFORMATION ", " INFO "
+      str_replace res,  " SOCIETY ", " SOC "
+      str_replace res,  " DEPARTMENT ", " DEPT "
+      str_replace res,  " REGIONAL ", " REG "
+      str_replace res,  " REGION ", " REG "
+      str_replace res,  " AUTHORITY ", " AUTH "
+      str_replace res,  " NATIONAL ", " NATL "
+      str_replace res,  " INTERNATIONAL ", " INT "
+      str_replace res,  " INTERNATION ", " INT "
+      str_replace res,  " INTL ", " INT "
+      str_replace res,  " MARKETING ", " MKT "
+      str_replace res,  " MKTG ", " MKT "
+      str_replace res,  " MANAGEMENT ", " MGT "
+      str_replace res,  " MGMT ", " MGT "
 
       res.strip!
       space_loc = res.index ' '
@@ -129,25 +186,40 @@ module InstanceMethods
          res.gsub! 'A ', ''
         end
 
-        # Remove last word if it is a company word
+        # Remove last word if it is a company word or &
         space_loc = res.index ' '
         if space_loc
           res_array = res.split(' ')
           if is_company_word(res_array.last)
-            res = res_array.first(res_array.size - 1).join(' ')
+            res_array.pop
+            if is_company_word(res_array.last) # Look at the new last word
+              res_array.pop
+            end
+            res = res_array.join(' ')
           end
+
+          res = res[0..res.size-2] if res.end_with? '&'
         end
       end
 
       res
     end
 
-    def is_company_word in_word
+    def is_company_word(in_word)
       if ["ADV","ADVERTISING","AGCY","AGENCY","AGY","ASC","ASS","ASSN","ASSOC","ASSOCIAT","ASSOCIATES","ASSOCIATION","ATTORNEY","ATTRNY","ATTY","ATY","AUTO","CO","COMP","COMPANIES","COMPANY","CORP","CORPORATION","CT","CONTRA","DEPARTMENT","DEPT","DIR","DIRECT","DIV","DIVISION","GROUP","HOLDINGS","INC","INCORPORATED","INT","LIMITED","LLC","LLP","LOCAL","LTD","PC","PLC","PROD","PRODS","PRODUCT","PRODUCTIONS","PRODUCTS","TR","TRADE"].include?(in_word)
         true
       else
         false
       end
+    end
+
+    # It's more than a gsub
+    def str_replace(orig, rep, with)
+      s = orig
+      while s.include?(rep)
+        s.sub! rep, with
+      end
+      orig
     end
   end
 end
